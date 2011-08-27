193497425cea7dfe273a083381646dca66cd7616
nathaniel.una <nathaniel.una@f367f39a-9a2f-8419-51fc-2c2402d2a6f9>
2011-08-27 15:16:08 +0000

    Criação da classe Emprestimo. falta implementar... só vou ter tempo depois das 10:00hs da noite... [se alguem quiser, fique a vontade xD ]

---
+++ trunk/Emprestimus/src/codigo/logica/itens/Emprestimo.java
@@ -0,0 +1,25 @@
+package codigo.logica.itens;
+
+import java.util.GregorianCalendar;
+
+import codigo.logica.pessoas.Usuario;
+
+/**
+ * Gerencia um emprestimo
+ * 
+ * @author Nathaniel
+ * @since 27/08/2011
+ * @version 1.0
+ */
+public class Emprestimo {
+	private Item item;
+	private Usuario credor; //O cara que empresta
+	private Usuario devedor; //O cara que pega emprestado
+	private GregorianCalendar dataDoEmprestimo;
+	private int duracao; //Prazo, em dias, para a devolucao do ítem.
+	
+	
+	
+	
+
+}
