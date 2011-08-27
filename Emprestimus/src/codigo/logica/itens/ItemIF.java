ed2179d17d0c54436aae4058ef197aba1fc79ba3
joeffisonsa <joeffisonsa@f367f39a-9a2f-8419-51fc-2c2402d2a6f9>
2011-08-27 09:15:37 +0000

    :P Cancei de comentar e ver um monte de comites sem comentários. kkkkkkkkk Acabei comentando! :D

--- Emprestimus/src/codigo/logica/itens/ItemIF.java
+++ Emprestimus/src/codigo/logica/itens/ItemIF.java
@@ -2,18 +2,21 @@ package codigo.logica.itens;
 
 
 /**
+ * Esta interface eh utilizada para representar itens que podem ser adicionados pelo usuario de um sistema
  * 
  * @author Nathaniel
+ * @author Joeffison Silverio de Andrade, joeffisonsa@gmail.com
  * @since 25/08/2011
- * @version 1.0
+ * @version 1.1
  */
 public interface ItemIF {
 	
-	void setNome(String nome);
-	void setDescricao(String descricao);
-	void setCategoria(ItemCategoria categoria);
+	public String getNome();
+	public String getDescricao();
+	public ItemCategoria getCategoria();
 	
-	String getNome();
-	String getDescricao();
-	ItemCategoria getCategoria();
+	public void setNome(String nome);
+	public void setDescricao(String descricao);
+	public void setCategoria(ItemCategoria categoria);
+	
 }
