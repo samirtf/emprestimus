b70ccebe385c4e91e6310468fa2df5a26d4f815b
samircc20092 <samircc20092@f367f39a-9a2f-8419-51fc-2c2402d2a6f9>
2011-08-29 23:47:20 +0000

    emprestimus class created

--- Emprestimus/src/controlador/Emprestimus.java
+++ Emprestimus/src/controlador/Emprestimus.java
@@ -1,12 +1,26 @@
 package controlador;
 
-public interface Emprestimus {
+public class Emprestimus implements EmprestimusIF{
+
+	@Override
+	public void criarUsuario(String login, String nome, String endereco) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int abrirSessao(String login) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getAtributoUsuario(String login, String atributo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 	
-	public void criarUsuario( String login, String nome, String endereco );
 	
-	public int abrirSessao( String login );
-	
-	public String getAtributoUsuario( String login, String atributo );
 
 }
