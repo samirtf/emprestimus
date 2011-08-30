d6ff6d8e80fc9c835d23163eac13df4733efec64
samircc20092 <samircc20092@f367f39a-9a2f-8419-51fc-2c2402d2a6f9>
2011-08-30 00:57:11 +0000

    controlador atualizado

--- trunk/Emprestimus/src/controlador/EmprestimusIF.java
+++ trunk/Emprestimus/src/controlador/EmprestimusIF.java
@@ -5,7 +5,7 @@ public interface EmprestimusIF {
 	
 	public void criarUsuario( String login, String nome, String endereco );
 	
-	public int abrirSessao( String login );
+	public Long abrirSessao( String login );
 	
 	public String getAtributoUsuario( String login, String atributo );
 	
