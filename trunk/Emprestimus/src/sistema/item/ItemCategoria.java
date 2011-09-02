376ce7aeeb4a1ebe0f50774333d63e190604bbec
joeffisonsa <joeffisonsa@f367f39a-9a2f-8419-51fc-2c2402d2a6f9>
2011-09-02 11:49:48 +0000

    Alterei Item, ItemIF e ItemCategoria. Adicionei ValidadorString (classe utilitaria) e Mensagem (enum). Espero não ter novas surpres. <facepalm>

--- trunk/Emprestimus/src/sistema/item/ItemCategoria.java
+++ trunk/Emprestimus/src/sistema/item/ItemCategoria.java
@@ -1,5 +1,54 @@
 package sistema.item;
 
+import sistema.utilitarios.Mensagem;
+import sistema.utilitarios.ValidadorString;
+
+/**
+ * Este enum serve para caracterizar itens segundo a sua categoria.
+ * 
+ * @author Joeffison Silverio de Andrade, 21011853, joeffisonsa@gmail.com
+ * @version 1.0
+ * @since 1.0
+ */
 public enum ItemCategoria {
-	LIVRO, FILME, JOGO;
+	LIVRO("Livro"), FILME("Filme"), JOGO("Jogo");
+	
+	private final String nome;
+	
+	private ItemCategoria(String nome) {
+		this.nome = nome;
+	}
+	
+	/**
+	 * A partir do nome da categoria, este metodo retorna o enum indicado.
+	 * 
+	 * @param categoria
+	 * 		Nome da categoria que deve ser retornada.
+	 * @return
+	 * 		Categoria indicada pela string passada.
+	 * @throws Exception
+	 * 		Caso a string, passada como parametro, nao represente nenhum dos enums, lanca excecao. 
+	 */
+	public static ItemCategoria getCategoria(String categoria) throws Exception {
+		if (!ValidadorString.validaString(categoria).equals(Mensagem.OK.getMensagem())) {
+			throw new Exception(Mensagem.CATEGORIA_INVALIDA.getMensagem());
+		}
+		
+		categoria.toUpperCase();
+		if (categoria.equals("LIVRO")) {
+			
+		} else if (categoria.equals("FILME")) {
+			
+		} else if (categoria.equals("JOGO")) {
+			
+		} else {
+			throw new Exception(Mensagem.CATEGORIA_INEXISTENTE.getMensagem());
+		}
+		return ItemCategoria.FILME;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+	
 }
--- trunk/Emprestimus/src/sistema/item/ItemIF.java
+++ trunk/Emprestimus/src/sistema/item/ItemIF.java
@@ -1,17 +1,25 @@
 package sistema.item;
 
+/**
+ * Esta interface eh utilizada para representar itens que podem ser adicionados pelo usuario de um sistema.
+ * 
+ * @author Joeffison Silverio de Andrade, 21011853, joeffisonsa@gmail.com
+ * @version 1.2
+ * @since 1.0
+ */
 public interface ItemIF {
 	
 	/**
-	 * Recupera o idItem do item.
+	 * Recupera o id do item.
 	 * 
 	 * @return
-	 * 		O idItem do objeto.
+	 * 		O id do objeto.
 	 */
 	public String getIdItem();
 	
 	/**
 	 * Recupera o nome do item.
+	 * 
 	 * @return
 	 * 		O nome do item.
 	 */
@@ -19,13 +27,15 @@ public interface ItemIF {
 	
 	/**
 	 * Recupera a categoria do item.
+	 * 
 	 * @return
 	 * 		A categoria do item.
 	 */
 	public String getCategoria();
 	
 	/**
-	 * Recupera o tipo categoria do item.
+	 * Recupera o tipo da categoria do item.
+	 * 
 	 * @return
 	 * 		O tipo da categoria do item.
 	 */
@@ -40,15 +50,17 @@ public interface ItemIF {
 	public String getDescricao();
 	
 	/**
-	 * Configura o nome do item.
+	 * Altera o nome do item.
 	 * 
 	 * @param nome
 	 * 		O nome do item.
+	 * @throws Exception
+	 * 		Caso o nome nao seja uma string valida. 
 	 */
-	public void setNome( String nome );
+	public void setNome( String nome ) throws Exception;
 	
 	/**
-	 * Configura a categoria do item.
+	 * Altera a categoria do item.
 	 * 
 	 * @param categoria
 	 * 		A categoria do item.
@@ -58,20 +70,24 @@ public interface ItemIF {
 	public void setCategoria( String categoria ) throws Exception;
 	
 	/**
-	 * Configura a categoria do item.
+	 * Altera a categoria do item.
 	 * 
 	 * @param categoria
 	 * 		A categoria do item (ItemCategoria).
+	 * @throws Exception
+	 * 		Caso o parametro passado seja nulo, ele lanca excecao. 
 	 */
-	public void setCategoria( ItemCategoria categoria );
+	public void setCategoria( ItemCategoria categoria ) throws Exception;
 	
 	/**
-	 * Configura a descricao do item.
+	 * Altera a descricao do item.
 	 * 
 	 * @param descricao
 	 * 		A descricao do item.
+	 * @throws Exception 
+	 * 		Lanca excecao, no caso de passar uma string invalida
 	 */
-	public void setDescricao( String descricao );
+	public void setDescricao( String descricao ) throws Exception;
 	
 	/**
 	 * Verifica se item estah disponivel.
@@ -83,13 +99,21 @@ public interface ItemIF {
 	public boolean estahDisponivel();
 	
 	/**
-	 * Verifica se a igualdade dos item.
+	 * Altera a disponibilidade do item.
+	 * 
+	 * @param disponibilidade
+	 * 		Nova condicao de disponibilidade para o item.
+	 */
+	public void setDisponibilidade(boolean disponibilidade);
+	
+	/**
+	 * Verifica-se a igualdade dos item.
 	 * 
 	 * @param outroItem
 	 * 		O outro item a ser comparado.
 	 * @return
 	 * 		True - Se os itens forem iguais. Neste caso, a comparacao eh
-	 *   realizada pelo idItem.
+	 *   realizada pelo id dos mesmos.
 	 *   	False - Se forem de instancias diferentes ou nao forem iguais.
 	 */
 	public boolean equals( Object outroItem );
