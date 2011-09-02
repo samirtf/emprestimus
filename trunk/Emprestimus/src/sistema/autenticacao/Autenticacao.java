5d2ba2c0406f2a4c2e0ba39eb8be1ea35df26fea
samircc20092 <samircc20092@f367f39a-9a2f-8419-51fc-2c2402d2a6f9>
2011-09-02 13:32:29 +0000

    comitando classe autenticacao

---
+++ trunk/Emprestimus/src/sistema/autenticacao/Autenticacao.java
@@ -0,0 +1,37 @@
+package sistema.autenticacao;
+
+public class Autenticacao implements AutenticacaoIF{
+	
+	
+
+	@Override
+	public void zerarSistema() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void encerrarSistema() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean criarUsuario(String login, String nome, String endereco) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String abrirSessao(String login) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAtributoUsuario(String login, String aributo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
