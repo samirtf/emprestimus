64eaa41b35e2fa907e5c081fc890136b9f7e7403
samircc20092 <samircc20092@f367f39a-9a2f-8419-51fc-2c2402d2a6f9>
2011-09-09 13:18:36 +0000

    testes de aceitação

---
+++ trunk/Emprestimus/src/testes/aceitacao/UserFacede1.java
@@ -0,0 +1,78 @@
+package testes.aceitacao;
+
+import iu.Emprestimus;
+import iu.EmprestimusIF;
+
+
+
+
+/**
+ * UFCG - CEEI - DSC- SI1
+ * Projeto da Disciplina Sistema de Informa��o 1 - 2011.2
+ * 
+ * Professor: Nazareno 
+ * 
+ * Alunos: Samir Trajano Feitosa - samircc20092@gmail.com
+ *                 nome completo - email
+ * 
+ */
+
+public class UserFacede1 {
+	
+	public EmprestimusIF sistema = new Emprestimus();
+	
+	/**
+	 * Cria usuario a partir de um login, um nome e um endereco.
+	 * @param login
+	 *     O login do usuario.
+	 * @param nome
+	 *     O nome do usuario.
+	 * @param endereco
+	 *     O endereco do usuario.
+	 */
+	public void criarUsuario(String login, String nome, String endereco) {
+		sistema.criarUsuario(login, nome, endereco);
+	}
+
+	/**
+	 * Abre uma sessao para o usuario.
+	 * @param login
+	 *     O login do usuario.
+	 * @return
+	 *     O id de sessao para o usuario.
+	 */
+	public String abrirSessao(String login) {
+		return sistema.abrirSessao(login);
+	}
+
+	/**
+	 * Recupera um atributo do usuario.
+	 * @param login
+	 *     O login do usuario.
+	 * @param atributo
+	 * 	   O atributo do usuario.
+	 * @return
+	 *     Uma representacao de um atributo do usuario.
+	 */
+	public String getAtributoUsuario(String login, String atributo) {
+		return sistema.getAtributoUsuario(login, atributo);
+	}
+	
+	/**
+	 * Metodo que zera o Sistema.
+	 * 
+	 */
+	public void zerarSistema() {
+		sistema.zerarSistema();
+	}
+
+	/**
+	 * Metodo que encerra o Sistema.
+	 * 
+	 */
+	public void encerrarSistema() {
+		sistema.encerrarSistema();
+	}
+
+}
+
