b19f46a43309cf177ab05735aaf2679faf53869b
samircc20092 <samircc20092@f367f39a-9a2f-8419-51fc-2c2402d2a6f9>
2011-09-09 17:35:32 +0000

    comitando ItemBD

---
+++ Emprestimus/src/sistema/persistencia/ItemBD.java
@@ -0,0 +1,19 @@
+package sistema.persistencia;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import sistema.item.ItemIF;
+
+public class ItemBD {
+	
+	private static int contadorID = 0;
+	
+	Map<String, ItemIF> itensCadastrados = new TreeMap<String, ItemIF>();
+	
+	
+	
+	
+	
+
+}
