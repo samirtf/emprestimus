7b5f8153e7af918e81fc1651b8d8acd7f154246c
samircc20092 <samircc20092@f367f39a-9a2f-8419-51fc-2c2402d2a6f9>
2011-09-09 15:32:52 +0000

    commit TestUS2

---
+++ Emprestimus/src/testes/aceitacao/TestUS2.java
@@ -0,0 +1,32 @@
+package testes.aceitacao;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import easyaccept.EasyAcceptFacade;
+
+public class TestUS2 {
+
+	public static void main(String[] args) throws Exception {
+		
+		run();
+		
+	}
+	
+	public static void run() {
+		
+		List<String> files = new ArrayList<String>();
+		String file1 = System.getProperty("user.dir") + "/src/testes/aceitacao/US02.txt";
+		files.add(file1);
+
+		UserFacede2 UserFacede = new UserFacede2();
+
+		EasyAcceptFacade eaFacade = new EasyAcceptFacade(UserFacede, files);
+
+		eaFacade.executeTests();
+
+		System.out.println(eaFacade.getCompleteResults());
+	}
+
+}
