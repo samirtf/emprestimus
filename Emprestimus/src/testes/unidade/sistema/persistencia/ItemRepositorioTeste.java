08a81b56077e80f73b0dc304e69076c9e1ab4a6b
samircc20092 <samircc20092@f367f39a-9a2f-8419-51fc-2c2402d2a6f9>
2011-09-13 16:24:20 +0000

    adicionando mais testes

---
+++ Emprestimus/src/testes/unidade/sistema/persistencia/ItemRepositorioTeste.java
@@ -0,0 +1,18 @@
+package testes.unidade.sistema.persistencia;
+
+public class ItemRepositorioTeste {
+	
+
+	public void setUp(){
+		System.out.println("Inicializando...");
+	}
+	
+	public void tearDown(){
+		System.out.println("Encerrando...");
+	}
+	
+	public void testa01(){
+		
+	}
+	
+}
