ba9014405dcdd35f550cb8cf7d0e282ffae34fa8
samircc20092 <samircc20092@f367f39a-9a2f-8419-51fc-2c2402d2a6f9>
2011-09-18 22:15:30 +0000

    comitando emprestimo

---
+++ Emprestimus/src/sistema/emprestimo/Emprestimo.java
@@ -0,0 +1,13 @@
+package sistema.emprestimo;
+import sistema.emprestimo.EmprestimoIF;
+import sistema.item.ItemIF;
+
+
+public class Emprestimo implements EmprestimoIF{
+	
+	String id, loginAmigo;
+	int duracao;
+	ItemIF item;
+	String tipo;
+
+}
