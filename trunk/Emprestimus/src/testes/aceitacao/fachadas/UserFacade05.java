9864015b9bf79eabf19d39827acacc02f66314bf
samircc20092 <samircc20092@f367f39a-9a2f-8419-51fc-2c2402d2a6f9>
2011-09-18 21:21:29 +0000

    atualizando user facade 05 =P

--- trunk/Emprestimus/src/testes/aceitacao/fachadas/UserFacade05.java
+++ trunk/Emprestimus/src/testes/aceitacao/fachadas/UserFacade05.java
@@ -1,5 +1,21 @@
 package testes.aceitacao.fachadas;
 
-public class UserFacade05 {
+public class UserFacade05 extends UserFacade04{
+	
+	public String getAmigos( String idSessao ) throws Exception{
+		return sistema.getAmigos(idSessao);
+	}
+	
+	public String getAmigos( String idSessao, String login ) throws Exception{
+		return sistema.getAmigos(idSessao, login);
+	}
+
+	public String getItens( String idSessao ) throws Exception{
+		return sistema.getItens(idSessao);
+	}
+	
+	public String getItens( String idSessao, String login ) throws Exception{
+		return sistema.getItens(idSessao, login);
+	}
 
 }
