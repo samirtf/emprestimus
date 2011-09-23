1ed2164094fe7fb83b80d48553bc48ce725312bc
nathaniel.una <nathaniel.una@f367f39a-9a2f-8419-51fc-2c2402d2a6f9>
2011-09-23 00:39:14 +0000

    coisa pouca nas 8 e 9

--- trunk/Emprestimus/src/testes/aceitacao/fachadas/UserFacade09.java
+++ trunk/Emprestimus/src/testes/aceitacao/fachadas/UserFacade09.java
@@ -1,16 +1,14 @@
 package testes.aceitacao.fachadas;
 
 public class UserFacade09 extends UserFacade08 {
-	
-	public void requisitarDevolucao(String idSessao, String idEmprestimo) throws Exception {
+
+	public void requisitarDevolucao(String idSessao, String idEmprestimo)
+			throws Exception {
 		sistema.requisitarDevolucao(idSessao, idEmprestimo);
 	}
-	
+
 	public void adicionarDias(String dias) throws Exception {
 		sistema.adicionarDias(dias);
-		//TODO implemente isto!
 	}
-	
-	
 
 }
