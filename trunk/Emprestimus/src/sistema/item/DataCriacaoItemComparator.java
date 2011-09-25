89cd09960ba29af868d1f09417f528bfa49ed620
samircc20092 <samircc20092@f367f39a-9a2f-8419-51fc-2c2402d2a6f9>
2011-09-25 03:21:20 +0000

    comitando comparadores

---
+++ trunk/Emprestimus/src/sistema/item/DataCriacaoItemComparator.java
@@ -0,0 +1,17 @@
+package sistema.item;
+
+import java.util.Date;
+import java.util.Comparator;
+
+public class DataCriacaoItemComparator implements Comparator<ItemIF>{
+
+	@Override
+	public int compare(ItemIF item1, ItemIF item2) {
+		
+		Date dataCriacaoItem1 = item1.getDataCriacao();        
+        Date dataCriacaoItem2 = item2.getDataCriacao();
+       
+        return dataCriacaoItem1.compareTo(dataCriacaoItem2);
+	}
+
+}
