b0e04670365e0b01088f4805becfccd82450fde6
joeffisonsa <joeffisonsa@f367f39a-9a2f-8419-51fc-2c2402d2a6f9>
2011-09-26 04:02:51 +0000

    Javadoc de 3 classes.

--- trunk/Emprestimus/src/sistema/utilitarios/Validador.java
+++ trunk/Emprestimus/src/sistema/utilitarios/Validador.java
@@ -7,18 +7,22 @@ import sistema.excecoes.ArgumentoInvalidoException;
  * algo.
  * 
  * @author José Nathaniel L. de Abrante - nathaniel.una@gmail.com
+ * @author Joeffison Silverio de Andrade, 21011853, joeffisonsa@gmail.com
  * @since 10/09/2011
  * @version 1.0
  */
 public class Validador {
 
 	/**
-	 * Testa se um dado objeto é nullo, passando-se uma mensagem de erro, que
+	 * Testa se um dado objeto é nulo, passando-se uma mensagem de erro, que
 	 * será acoplada a uma ArgumentoInvalidoException
 	 * 
 	 * @param obj
+	 * 		Objeto a ser verificado.
 	 * @param mensagem
+	 * 		Mensagem da exceção gerada em caso do objeto ser nulo.
 	 * @throws ArgumentoInvalidoException
+	 * 		Exceção gerada em caso do objeto ser nulo.
 	 */
 	public static void assertNaoNulo(Object obj, String mensagem)
 			throws ArgumentoInvalidoException {
@@ -27,13 +31,16 @@ public class Validador {
 	}
 
 	/**
-	 * Testa se uma dada String é vazia ou se é formada apenas por espaços,
+	 * Testa se uma dada {@link String} é vazia ou se é formada apenas por espaços,
 	 * passando-se uma mensagem de erro, que será acoplada a uma
 	 * ArgumentoInvalidoException
 	 * 
 	 * @param str
+	 * 		String a ser testada.
 	 * @param mensagem
+	 * 		Mensagem da exceção gerada em caso da String for inválida segundo os requisitos acima.
 	 * @throws ArgumentoInvalidoException
+	 * 		Exceção gerada em caso da String for inválida segundo os requisitos acima.
 	 */
 	public static void assertStringNaoVazia(String str, String mensagem)
 			throws ArgumentoInvalidoException {
@@ -46,8 +53,11 @@ public class Validador {
 	 * ArgumentoInvalidoException.
 	 * 
 	 * @param bool
+	 * 		Boolean a ser verificado.
 	 * @param mensagem
+	 * 		Mensagem para a exceção gerada, se for o caso.
 	 * @throws ArgumentoInvalidoException
+	 * 		Exceção gerada em caso do boolean ser false.
 	 */
 	public static void asserteTrue(boolean bool, String mensagem)
 			throws ArgumentoInvalidoException {
