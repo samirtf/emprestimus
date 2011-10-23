56923c6fda207e093432eee251a4df44867dede5
joeffisonsa <joeffisonsa@f367f39a-9a2f-8419-51fc-2c2402d2a6f9>
2011-10-23 13:17:59 +0000

    Historico e nuances.

---
+++ Emprestimus/src/sistema/notificacao/UsuarioIFListenner.java
@@ -0,0 +1,9 @@
+package sistema.notificacao;
+
+public interface UsuarioIFListenner{
+
+	public void notificaNovaAmizade(Notificacao notificacao);
+	
+	public void notificaAmizadeRemovida(Notificacao notificacao);
+	
+}
