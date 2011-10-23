b99f6ae86d65936e14bf20fc1dd17215eb01be4a
samircc20092 <samircc20092@f367f39a-9a2f-8419-51fc-2c2402d2a6f9>
2011-10-23 23:19:00 +0000

    atualizando fachada 15

--- trunk/Emprestimus/src/testes/aceitacao/fachadas/UserFacade15.java
+++ trunk/Emprestimus/src/testes/aceitacao/fachadas/UserFacade15.java
@@ -1,7 +1,6 @@
 package testes.aceitacao.fachadas;
 
 public class UserFacade15 extends UserFacade14 {
-	
 	public String historicoAtividades(String idSessao) throws Exception {
 		return sistema.historicoAtividades(idSessao);
 	}
