2cd865cef68432fff08540692148f961c63c231a
samircc20092 <samircc20092@f367f39a-9a2f-8419-51fc-2c2402d2a6f9>
2011-10-23 23:03:51 +0000

    corrigindo inicialização de rede de relacionamentos

--- Emprestimus/src/sistema/autenticacao/Autenticacao.java
+++ Emprestimus/src/sistema/autenticacao/Autenticacao.java
@@ -14,6 +14,7 @@ import sistema.excecoes.ArgumentoInvalidoException;
 import sistema.item.AcervoDeItens;
 import sistema.item.ItemIF;
 import sistema.mensagem.Correio;
+import sistema.usuario.RelacionamentosUsuarios;
 import sistema.usuario.Usuario;
 import sistema.usuario.UsuarioIF;
 import sistema.utilitarios.Mensagem;
@@ -96,6 +97,7 @@ public class Autenticacao implements AutenticacaoIF {
 		Correio.adicionaCaixaPostalAoUsuario(login);
 		BancoDeEmprestimos.getInstance().adicionaContaAoUsuario(login);
 		AcervoDeItens.getInstance().adicionaBauhAoUsuario(login);
+		RelacionamentosUsuarios.getInstance().adicionaCicloDeAmizadeAoUsuario(login);
 	}
 
 	@Override
