772169b35acec70284e5127d5f2d33669f055638
samircc20092 <samircc20092@f367f39a-9a2f-8419-51fc-2c2402d2a6f9>
2011-10-23 23:22:50 +0000

    atualizando fachadas 15 e 16

--- trunk/Emprestimus/src/testes/aceitacao/fachadas/UserFacade15.java
+++ trunk/Emprestimus/src/testes/aceitacao/fachadas/UserFacade15.java
@@ -1,6 +1,7 @@
 package testes.aceitacao.fachadas;
 
 public class UserFacade15 extends UserFacade14 {
+	
 	public String historicoAtividades(String idSessao) throws Exception {
 		return sistema.historicoAtividades(idSessao);
 	}
--- trunk/Emprestimus/src/testes/aceitacao/fachadas/UserFacade16.java
+++ trunk/Emprestimus/src/testes/aceitacao/fachadas/UserFacade16.java
@@ -1,5 +1,9 @@
 package testes.aceitacao.fachadas;
 
-public class UserFacade16 {
-
+public class UserFacade16 extends UserFacade15 {
+	
+	public String historicoAtividadesConjunto(String idSessao) throws Exception {
+		return sistema.historicoAtividades(idSessao);
+	}
+	
 }
