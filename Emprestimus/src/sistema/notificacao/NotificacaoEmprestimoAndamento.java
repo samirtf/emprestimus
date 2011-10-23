d3c1643b346784ef1b94466941c6beb13fd4c8ae
nathaniel.una <nathaniel.una@f367f39a-9a2f-8419-51fc-2c2402d2a6f9>
2011-10-23 15:04:21 +0000

    Operações sobre as notificações... falta terminar!

--- Emprestimus/src/sistema/notificacao/NotificacaoEmprestimoAndamento.java
+++ Emprestimus/src/sistema/notificacao/NotificacaoEmprestimoAndamento.java
@@ -1,28 +1,36 @@
 package sistema.notificacao;
 
 import java.util.Date;
-import java.util.GregorianCalendar;
 
+import sistema.item.ItemIF;
 import sistema.usuario.Usuario;
 import sistema.usuario.UsuarioIF;
 
 /**
  * 
  * @author José Nathaniel L de Abrante - nathaniel.una@gmail.com
- * @since 20/11/2011
+ * @since 23/11/2011
  * @version 1.0
  *
  */
-public class NotificacaoNovoAmigo implements Notificacao {
-	private UsuarioIF usuario1;
-	private UsuarioIF usuario2;
+public class NotificacaoEmprestimoAndamento implements Notificacao {
+	private UsuarioIF dono;
+	private UsuarioIF beneficiado;
+	private ItemIF item;
+
 	private Date data;
 	private Long id;
 
-	public NotificacaoNovoAmigo(UsuarioIF usuario1, UsuarioIF usuario2) {
-		this.usuario1 = usuario1;
-		this.usuario2 = usuario2;
-		this.data = new GregorianCalendar().getTime();
+	/**
+	 * @param dono
+	 * @param beneficiado
+	 * @param item
+	 */
+	public NotificacaoEmprestimoAndamento(UsuarioIF dono,
+			UsuarioIF beneficiado, ItemIF item) {
+		this.dono = dono;
+		this.beneficiado = beneficiado;
+		this.item = item;
 	}
 
 	@Override
@@ -38,12 +46,8 @@ public class NotificacaoNovoAmigo implements Notificacao {
 
 	@Override
 	public String getMensagem(Usuario usuario) {
-		if (usuario1.equals(usuario)) {
-			return usuario1.getNome() + " e " + usuario2.getNome()
-					+ " são amigos agora";
-		}
-		return usuario2.getNome() + " e " + usuario1.getNome()
-				+ " são amigos agora";
+		return dono.getNome() + " emprestou " + item.getNome() + " a "
+				+ beneficiado.getNome();
 	}
 
 	@Override
@@ -59,8 +63,8 @@ public class NotificacaoNovoAmigo implements Notificacao {
 
 	@Override
 	public boolean equals(Object obj) {
-		if (obj instanceof NotificacaoNovoAmigo) {
-			NotificacaoNovoAmigo notificacao = (NotificacaoNovoAmigo) obj;
+		if (obj instanceof NotificacaoEmprestimoAndamento) {
+			NotificacaoEmprestimoAndamento notificacao = (NotificacaoEmprestimoAndamento) obj;
 			return id.equals(notificacao.getId());
 		}
 		return false;
