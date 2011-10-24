0943c5faca89322b143e128994441080ae07742d
samircc20092 <samircc20092@f367f39a-9a2f-8419-51fc-2c2402d2a6f9>
2011-10-24 16:22:01 +0000

    refatora item

--- trunk/Emprestimus/src/sistema/notificacao/UsuarioIFListenner.java
+++
@@ -1,9 +0,0 @@
-package sistema.notificacao;
-
-public interface UsuarioIFListenner{
-
-	public void notificaNovaAmizade(Notificacao notificacao);
-	
-	public void notificaAmizadeRemovida(Notificacao notificacao);
-	
-}
