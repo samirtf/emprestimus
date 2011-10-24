9a69f6b2df3263fe6b6f22b7e04fa33ad7437319
samircc20092 <samircc20092@f367f39a-9a2f-8419-51fc-2c2402d2a6f9>
2011-10-24 00:02:06 +0000

    atualizando fachadas

--- Emprestimus/src/testes/aceitacao/fachadas/UserFacade17_18.java
+++ Emprestimus/src/testes/aceitacao/fachadas/UserFacade17_18.java
@@ -1,5 +1,16 @@
 package testes.aceitacao.fachadas;
 
-public class UserFacade17_18 {
-
+public class UserFacade17_18 extends UserFacade16 {
+	
+	public String publicarPedido(String idSessao, String nomeItem, String descricaoItem) throws Exception{
+		return sistema.publicarPedido(idSessao, nomeItem, descricaoItem);
+	}
+	
+	public void oferecerItem(String idSessao, String idPublicacaoPedido, String idItem) throws Exception {
+		sistema.oferecerItem(idSessao, idPublicacaoPedido, idItem);
+	}
+	
+	public void rePublicarPedido(String idSessao, String idPublicacaoPedido) throws Exception {
+		sistema.republicarPedido(idSessao, idPublicacaoPedido);
+	}
 }
